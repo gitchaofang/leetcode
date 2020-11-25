5781cdde6eec8e0d51bbc3228711873a03073ac6
Chao Fang <fangchao118@gmail.com>
2020-11-25 15:15:30 -0600

    Longest Increasing Path in a Matrix

--- 329_dfs.cpp
+++ 329_dfs.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
-    int dfs(const std::vector<std::vector<int>>& matrix, std::vector<std::vector<int>>& dp, const int& i, const int& j){
-        int res = 0;
+    int dfs(const std::vector<std::vector<int>>& matrix, const int& cur, std::vector<int>& record){
         int row = matrix.size();
         int col = matrix[0].size();
+        if(record[cur] != -1) return record[cur];
+        int res = 1;
         for(const std::pair<int,int>& dir: dire){
-            int x = i + dir.first;
-            int y = j + dir.second;
-            if(x < 0 || x >= row || y < 0 || y >= col || matrix[x][y] <= matrix[i][j]) continue;
-            if(dp[x][y] != -1) res = std::max(dp[x][y], res);
-            else res = std::max(res,dfs(matrix,dp,x,y));
+            int x = dir.first + cur / col;
+            int y = dir.second + cur % col;
+            if(x < 0 || x >= row || y < 0 || y >= col || matrix[x][y] <= matrix[cur / col][cur % col]) continue;
+            res = std::max(res, 1 + dfs(matrix, x * col + y, record));
         }
-        dp[i][j] = res + 1;
-        return res + 1;
+        record[cur] = res;
+        return res;
     }
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int row = matrix.size();
         if(row == 0) return 0;
         int col = matrix[0].size();
-        std::vector<std::vector<int>> dp(row,  std::vector<int>(col,-1));
-        int res = 0;
-        dire = {{0,-1},{-1,0},{1,0},{0,1}};
-        for(int i = 0; i < row; ++i){
-            for(int j = 0; j < col; ++j){
-                if(dp[i][j] != -1) res = std::max(dp[i][j],res);
-                else res = std::max(res,dfs(matrix, dp, i,j));
-            }
-        }
+        std::vector<int> record(row * col, -1);
+        dire = {{0,-1},{-1,0},{0,1},{1,0}};
+        int res = 1;
+        for(int i = 0; i < row * col; ++i)res = std::max(res, dfs(matrix, i, record));
         return res;
     }
 private:
-    std::vector<std::pair<int,int>> dire;
+    std::vector<std::pair<int,int>>dire;
 };
