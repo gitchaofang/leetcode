b708fc355f9a1e71acd6ba626dffa92f7d182ff2
Chao Fang <fangchao118@gmail.com>
2020-11-25 23:08:15 -0600

    Search a 2D Matrix

---
+++ 74.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        if(row == 0) return false;
+        int col = matrix[0].size();
+        int left = 0;
+        int right = row * col - 1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(matrix[mid / col][mid % col] == target) return true;
+            if(matrix[mid / col][mid % col] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return false;
+    }
+};
