12c68ef88dded99903991c973bab791c84797c22
Chao Fang <fangchao118@gmail.com>
2020-12-03 18:52:15 -0600

    Car Fleet

--- 853_1.cpp
+++ 853_1.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        if(position.size() == 0) return 0;
-        std::vector<std::pair<double,double>> v;
-        for(int i = 0; i < speed.size(); ++i) v.push_back({position[i], speed[i]});
-        std::sort(v.begin(), v.end());
-        double head = (target - v.back().first) / v.back().second;
-        int res = 0;
-        for(int i = v.size() - 1; i >= 0; --i){
-            double t = (target - v[i].first) / v[i].second;
-            if(t <= head) continue;
-            ++res;
-            head = t;
+        int n = position.size();
+        if(n <= 1) return n;
+        std::vector<std::pair<int,double>>v;
+        for(int i = 0; i < n; ++i){
+            v.push_back({position[i], double(target - position[i]) / double(speed[i])});
         }
-        return res + 1;
+        std::sort(v.begin(),v.end());
+        for(int i = v.size() - 2; i >= 0; --i){
+            if(v[i].second <= v[i + 1].second){
+                --n;
+                v[i].second = v[i + 1].second;
+            }
+        }
+        return n;
     }
 };
