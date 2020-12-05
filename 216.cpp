e95afb06a573e5fc830e4c074e274962bf8312b0
Chao Fang <fangchao118@gmail.com>
2020-12-05 17:43:39 -0600

    Combination Sum III

--- 216.cpp
+++ 216.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    std::vector<std::vector<int>> dfs(int k, const int& targ, const int& pos){
-        if(k == 1 && targ >= pos && targ <= 9) return{{targ}};
+    std::vector<std::vector<int>> dfs(const int& pos, const int& k, const int& n){
+        if(k == 1){
+            if(pos == n) return {{pos}};
+            return {};
+        }
         std::vector<std::vector<int>> res;
-        for(int i = pos; i <= 9; ++i){
-            if(i > targ) return res;
-            std::vector<std::vector<int>> v = dfs(k - 1, targ - i, i + 1);
-            if(v.empty()) continue;
-            for(std::vector<int>& sv: v){
-                sv.insert(sv.begin(), i);
-                res.push_back(sv);
+        for(int i = pos + 1; i <= 9; ++i){
+            if(i + pos > n) break;
+            std::vector<std::vector<int>> v = dfs(i, k - 1, n - pos);
+            for(std::vector<int>& tv: v){
+                tv.insert(tv.begin(), pos);
+                res.push_back(tv);
             }
         }
         return res;
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        return dfs(k, n, 1);
+        std::vector<std::vector<int>> res;
+        for(int i = 1; i <= 9; ++i){
+            std::vector<std::vector<int>> v = dfs(i, k, n);
+            if(!v.empty()) res.insert(res.end(),v.begin(),v.end());
+        }
+        return res;
     }
 };
