ee7b8555aad34a82a9cbc06dc5d5db520c9b245e
Chao Fang <fangchao118@gmail.com>
2020-12-04 20:25:18 -0600

    435.

--- 435.cpp
+++ 435.cpp
@@ -1,20 +1,17 @@
-lass Solution {
+class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        std::map<int,int>m;
-        for(const std::vector<int>& v: intervals){
-            if(m.find(v[0]) == m.end()) m[v[0]] = v[1];
-            else m[v[0]] = std::min(m[v[0]], v[1]);
-        }
-        int pre = INT_MIN;
-        int cnt = m.size();
-        for(auto it = m.begin(); it != m.end(); ++it){
-            if(it -> first < pre){ 
-                --cnt;
-                pre = std::min(it -> second, pre);
+        if(intervals.empty()) return 0;
+        std::sort(intervals.begin(), intervals.end());
+        int end = intervals[0][0];
+        int res = 0;
+        for(int i = 0; i < intervals.size(); ++i){
+            if(intervals[i][0] >= end) end = intervals[i][1];
+            else {
+                ++res;
+                end = std::min(end,intervals[i][1]);
             }
-            else pre = it -> second;
         }
-        return intervals.size() - cnt;
+        return res;
     }
 };
