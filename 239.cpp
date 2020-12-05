8289ed846de610226ba2f4acd8112d54ef68a550
Chao Fang <fangchao118@gmail.com>
2020-12-05 14:22:01 -0600

    Sliding Window Maximum

--- 239.cpp
+++ 239.cpp
@@ -3,14 +3,11 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         std::deque<int> q;
         std::vector<int> res;
-        int left = 0;
         for(int i = 0; i < nums.size(); ++i){
             while(!q.empty() && q.back() < nums[i]) q.pop_back();
             q.push_back(nums[i]);
-            while(i - left + 1 > k){
-                if(nums[left++] == q.front()) q.pop_front();
-            }
-            if(i - left + 1 == k) res.push_back(q.front());
+            if(i >= k && q.front() == nums[i - k]) q.pop_front();
+            if(i >= k - 1) res.push_back(q.front()); 
         }
         return res;
     }
