633f3ced0afc0471cfa98a563650df973a66730a
Chao Fang <fangchao118@gmail.com>
2020-12-05 14:07:07 -0600

    Strobogrammatic Number III

--- 248.cpp
+++ 248.cpp
@@ -3,27 +3,18 @@ public:
     int strobogrammaticInRange(string low, string high) {
         std::vector<std::pair<char,char>> dig({{'0','0'},{'1','1'},{'6','9'},{'8','8'},{'9','6'}});
         int cnt = 0;
+        std::queue<std::string> q({"","0","1","8"});
         if(low == "0"){
-            cnt = 1;
+            ++cnt;
             low = "1";
         }
-        std::queue<std::string> q({""});
-        for(int i = 0; i <= 3; ++i){
-            if(i == 2) continue;
-            if(high.size() > 1 || dig[i].first - '0' <= high[0] - '0') q.push(std::string(1,dig[i].first));
-        }
         while(!q.empty()){
-            for(int s = q.size(); s > 0; --s){
-                std::string str = q.front();
-                q.pop();
-                if(str[0] != '0' && ((str.size() == low.size() && str >= low) || (str.size() > low.size()))){ 
-                    ++cnt;
-                }
-                for(const std::pair<char,char>& p: dig){
-                    std::string s = p.first + str + p.second;
-                    if(s.size() > high.size() || (s.size() == high.size() && s > high)) break;
-                    q.push(s);
-                }
+            std::string str = q.front();
+            q.pop();
+            if(str != "" && str[0] != '0' && (str.size() > low.size() ||(str.size() == low.size() && str >= low)) && (str.size() < high.size() || (str.size() == high.size() && str <= high))) ++cnt;
+            for(const std::pair<char,char>& p: dig){
+                std::string next = p.first + str + p.second;
+                if(next.size() <= high.size()) q.push(next);
             }
         }
         return cnt;
