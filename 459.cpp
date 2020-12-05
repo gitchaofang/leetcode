328d7c4e7fd74ff9136bf398cd88c3ce1d5cc2cb
Chao Fang <fangchao118@gmail.com>
2020-12-04 20:03:24 -0600

    Repeated Substring Pattern

--- 459.cpp
+++ 459.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
         int n = s.size();
-        for(int i = 1; i <= s.size() / 2; ++i){
+        for(int i = 1; i <= s.size()/2; ++i){
             if(n % i != 0) continue;
             std::string str = s.substr(i) + s.substr(0,i);
             if(str == s) return true;
